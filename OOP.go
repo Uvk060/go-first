85d784ac7c9042770f7d2f166c302e69f128f80c
Uvk060 <uvk060@gmail.com>
2020-11-22 19:49:59 +0200

    some commit

--- OOP.go
+++ OOP.go
@@ -9,6 +9,13 @@ type MyObject struct {
 
 func main() {
 	thatObject := new(MyObject)          //link to the object
-	thatObject.MyString = "GO language." //эта строка равна тому то и в принте это отражается + ссылка на объект
-	fmt.Println(thatObject)              //link to the object
+	thatObject.MyString = "GO language." //эта строка равна тому-то и в принте это отражается + ссылка на объект
+	//thatObject := MyObject{
+	//MyString: "Hello"
+	//}      //так мы получаем сразу значение объекта, а через new сначала ссылку //this way is BETTER
+	fmt.Println(thatObject) //link to the object
+}
+
+func (m *MyObject) ChangeString(str string) { //метод к данному типу. но он не меняет исходный тип! только его копию
+	fmt.Println(m.MyString, str)
 }
